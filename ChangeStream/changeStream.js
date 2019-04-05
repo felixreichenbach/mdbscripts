7a01bc2e5b158bfa5f5a7b510d4f70f6714cc37e
felixreichenbach <felix.reichenbach@mongodb.com>
2019-04-05 19:02:18 +0200

    changestream configured

---
+++ ChangeStream/changeStream.js
@@ -0,0 +1,29 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+
+// MongoDB Connection Parameters
+
+const url = "mongodb+srv://user:password@hostname/?retryWrites=true"
+const dbName = "test";
+const colName = "changestream";
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+  const collection = db.collection(colName);
+  const changeStream = collection.watch();
+  
+  changeStream.on('change', next => {
+    // process next document
+    console.log("Stream!");
+  });
+
+  //client.close();
+});
