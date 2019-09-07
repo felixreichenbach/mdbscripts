5a06b355bee3fa0a0b60ba42e33b08896aae8a85
felixreichenbach <felix.reichenbach@mongodb.com>
2019-09-08 01:56:10 +0200

    filter added

--- graphqlserver/server.js
+++ graphqlserver/server.js
@@ -1,47 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+const { ApolloServer, gql } = require('apollo-server');
+// Construct a schema, using GraphQL schema language
 
-// Some fake data
-const books = [
-  {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
+const customers = [{ "name": "Felix", "lastname": "Reichenbach" }, { "name": "Simon", "lastname": "Reichenbach" },{"name": "Felix"}];
 
-// The GraphQL schema in string form
-const typeDefs = `
-  type Query { books: [Book] }
-  type Book { title: String, author: String }
+
+const typeDefs = gql`
+  type Query {
+    customers(name:String): [Customer]
+  }
+
+  type Customer {
+    name: String
+    lastname: String
+  }
 `;
-
-// The resolvers
+// Provide resolver functions for your schema fields
 const resolvers = {
-  Query: { books: () => books },
+  Query: {
+    customers(parent, args, context, info) {
+      return customers.filter(function (customer) {
+        return customer.name == args.name;
+      });
+    }
+  }
 };
 
-// Put together a schema
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
-});
-
-// Initialize the app
-const app = express();
-
-// The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
-// GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
 });
