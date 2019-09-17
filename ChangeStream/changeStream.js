a57e1e347329e44a34c29329cbf55998de3f3f3f
felixreichenbach <felix.reichenbach@mongodb.com>
2019-09-17 13:36:11 +0200

    Update changeStream.js

--- ChangeStream/changeStream.js
+++ ChangeStream/changeStream.js
@@ -4,26 +4,30 @@ const assert = require('assert');
 
 // MongoDB Connection Parameters
 
-const url = "mongodb+srv://demo:demo@hostname.provider.mongodb.net/test?retryWrites=true&w=majority"
+const url = "mongodb+srv://demo:<password>@hostname.provider.mongodb.net/test?retryWrites=true&w=majority"
 const dbName = "demo";
 const colName = "changestream";
+
+const pipeline = [
+    { $match: { 'fullDocument.username': 'agnes' } },
+    { $addFields: { 'newField': 'this is an added field!' } }
+];
 
 // Create a new MongoClient
 const client = new MongoClient(url, { useNewUrlParser: true });
 
 // Use connect method to connect to the Server
 client.connect(function (err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
 
-  const db = client.db(dbName);
-  const collection = db.collection(colName);
-  const changeStream = collection.watch();
-  
-  changeStream.on('change', next => {
-    // process next document
-    console.log("New Customer:"+next.fullDocument.name);
-  });
+    const db = client.db(dbName);
+    const collection = db.collection(colName);
+    const changeStream = collection.watch(pipeline, { fullDocument: 'updateLookup' });
 
-  //client.close();
+    changeStream.on('change', next => {
+        // process next document
+        console.log("Change Event:\n" + JSON.stringify(next, null, 4));
+    });
+
 });
